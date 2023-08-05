f1eb0c56404d4db50837c572e713997d9f5843bc
shreydan <iamshreydan@gmail.com>
2023-08-06 00:42:39 +0530

    start from a pretrained model

--- flamby/datasets/fed_covid19/model.py
+++ flamby/datasets/fed_covid19/model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class Baseline(nn.Module):
     def __init__(self,num_classes=2):
         super().__init__()
-        self.model = create_model('resnet18d',num_classes=num_classes,pretrained=False)
+        self.model = create_model('resnet18d',num_classes=num_classes,pretrained=True)
     
     def forward(self,x):
         return self.model(x)
