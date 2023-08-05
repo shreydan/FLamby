79b41a8a615a3ff7dc8f526afce95661c28d9700
shreydan <iamshreydan@gmail.com>
2023-08-05 23:13:14 +0530

    use sklearn.f1_score for metric

--- flamby/datasets/fed_covid19/metric.py
+++ flamby/datasets/fed_covid19/metric.py
@@ -1,9 +1,8 @@
-import torch
-from torchmetrics.functional import f1_score
+import numpy as np
+from sklearn.metrics import f1_score
 
 def metric(predictions,gt):
-    preds = torch.argmax(predictions, dim=1)
-    # auroc = roc_auc_score(y_true=gt.numpy().flatten(),y_score=preds.numpy().flatten())
-    f1 = f1_score(preds=predictions.squeeze(),target=gt.squeeze(),task='binary')
-#     cfm = confusion_matrix(y_true=gt.numpy().flatten(),y_pred=preds.flatten(),labels=list(range(2)))
+    predictions = np.argmax(predictions,axis=1).flatten()
+    gt = gt.flatten()
+    f1 = f1_score(y_true=gt,y_pred=predictions)
     return f1
