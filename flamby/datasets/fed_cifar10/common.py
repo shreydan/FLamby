ae12ef772e3e44031f3610d29768cc466953451f
shreydan <iamshreydan@gmail.com>
2023-10-06 20:02:51 +0530

    update #samples + epoch = 1

--- flamby/datasets/fed_cifar10/common.py
+++ flamby/datasets/fed_cifar10/common.py
@@ -4,7 +4,7 @@ from flamby.datasets.fed_cifar10.dataset import FedCIFAR10
 
 NUM_CLIENTS = 10
 BATCH_SIZE = 16
-NUM_EPOCHS_POOLED = 10
+NUM_EPOCHS_POOLED = 1
 LR = 1e-3
 Optimizer = torch.optim.Adam
 
@@ -12,5 +12,5 @@ FedClass = FedCIFAR10
 
 def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
     # setting num_updates=100
-    n_samples = 7496
+    n_samples = 5000
     return (n_samples // NUM_CLIENTS // batch_size) * NUM_EPOCHS_POOLED // num_updates
