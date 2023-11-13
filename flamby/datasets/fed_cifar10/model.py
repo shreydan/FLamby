a05c5d4ea3a7557ddf3ae79d09a8bc84d482739a
shreydan <iamshreydan@gmail.com>
2023-11-13 15:54:15 +0530

    results

--- flamby/datasets/fed_cifar10/model.py
+++ flamby/datasets/fed_cifar10/model.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 #         return self.model(x)
     
 class Baseline(nn.Module):
-    def __init__(self, num_classes=8, freeze_up_to_layer=46): #experiment with 45,54,60
+    def __init__(self, num_classes=10, freeze_up_to_layer=46): #experiment with 45,54,60
         super().__init__()
         self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
